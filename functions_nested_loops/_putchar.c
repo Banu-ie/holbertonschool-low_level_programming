fc26a987ed6f4480194044736819179168cfd83b
Banu-ie <i.banu.4757@gmail.com>
2025-12-02 09:17:31 +0000

    J

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -2,10 +2,9 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
+ * @c: character to print
  *
- * Return: On success 1.
- * On error, -1 is returned.
+ * Return: On success 1, on error -1
  */
 int _putchar(char c)
 {
