4890f71b9cb2e4c0b7a18b759f7155a591ff1adc
Banu-ie <i.banu.4757@gmail.com>
2025-12-02 09:33:21 +0000

    3

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -3,6 +3,7 @@
 
 int _putchar(char c);
 void print_alphabet(void);
-void print_alphabet_x10(void);  /* new function prototype */
+void print_alphabet_x10(void);
+int _islower(int c); /* prototype for _islower */
 
 #endif /* MAIN_H */
