ceb549da35500af737b7f683fb0274c4fffeae57
Banu-ie <i.banu.4757@gmail.com>
2025-12-03 08:30:51 +0000

    Add 0-isupper.c

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,40 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* _putchar prototype */
+int _putchar(char c);
+
+/* 0. isupper */
+int _isupper(int c);
+
+/* 1. isdigit */
+int _isdigit(int c);
+
+/* 2. Multiplication */
+int mul(int a, int b);
+
+/* 3. Print numbers 0-9 */
+void print_numbers(void);
+
+/* 4. Print numbers 0-9 excluding 2 and 4 */
+void print_most_numbers(void);
+
+/* 5. Print numbers 0-14 ten times */
+void more_numbers(void);
+
+/* 6. Print straight line */
+void print_line(int n);
+
+/* 7. Print diagonal line */
+void print_diagonal(int n);
+
+/* 8. Print square */
+void print_square(int size);
+
+/* 9. FizzBuzz */
+void fizz_buzz(void);
+
+/* 10. Print triangle */
+void print_triangle(int size);
+
+#endif /* MAIN_H */
