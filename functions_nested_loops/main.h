f55ec9cb114b6bd219b4e2fb1630a54d603d3bc2
Banu-ie <i.banu.4757@gmail.com>
2025-12-03 07:51:01 +0000

    Add 5-sign.c

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,11 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Functions prototypes */
+/* Function prototypes */
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
+int print_sign(int n); /* prototype for print_sign */
 
 #endif /* MAIN_H */
