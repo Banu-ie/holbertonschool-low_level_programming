5966a5fbc169dfa461092b0306c8cd635d9ad5e1
Banu-ie <i.banu.4757@gmail.com>
2025-12-09 08:16:28 +0000

    Add 6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,11 +6,16 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	if (!str)
+		return;
+
+	while (str[i])
 	{
 		_putchar(str[i]);
+		i += 2;
 	}
 	_putchar('\n');
 }
+
