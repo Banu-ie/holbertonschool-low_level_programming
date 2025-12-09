ae91a20a6f2ce1d92ef4cfa5d9cdb936cbdc34cc
Banu-ie <i.banu.4757@gmail.com>
2025-12-09 08:10:56 +0000

    Add 4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,16 +8,16 @@
  */
 void print_rev(char *s)
 {
-    int i;
+	int i;
 
-    /* Find length of string */
-    for (i = 0; s[i] != '\0'; i++)
-        ;
+	/* find the length of the string */
+	for (i = 0; s[i] != '\0'; i++)
+		;
 
-    /* Print characters in reverse order */
-    for (i = i - 1; i >= 0; i--)
-        _putchar(s[i]);
+	/* print the string in reverse */
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i]);
 
-    _putchar('\n');
+	_putchar('\n');
 }
 
