ae856404a8a5707f1f7d2fbaae59e42fb7abd2ee
Banu-ie <i.banu.4757@gmail.com>
2025-12-09 07:59:14 +0000

    Add 3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * _puts - prints a string followed by a new line
@@ -7,12 +7,12 @@
  */
 void _puts(char *str)
 {
-    int i = 0;
+    int len = 0;
 
-    while (str[i] != '\0')
-    {
-        putchar(str[i]);
-        i++;
-    }
-    putchar('\n');
+    while (str[len] != '\0')
+        len++;
+
+    write(1, str, len);
+    write(1, "\n", 1);
 }
+
