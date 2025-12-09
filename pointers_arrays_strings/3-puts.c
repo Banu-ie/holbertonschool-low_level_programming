0c40c68934a48245c465156bd03a1bd02c29c231
Banu-ie <i.banu.4757@gmail.com>
2025-12-09 08:00:08 +0000

    Add 3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -3,16 +3,19 @@
 
 /**
  * _puts - prints a string followed by a new line
- * @str: pointer to the string
+ * @str: pointer to the string to print
+ *
+ * Description: This function prints the string to stdout
+ *              using the write system call, then prints a newline.
  */
 void _puts(char *str)
 {
-    int len = 0;
+	int len = 0;
 
-    while (str[len] != '\0')
-        len++;
+	while (str[len] != '\0')
+		len++;
 
-    write(1, str, len);
-    write(1, "\n", 1);
+	write(1, str, len);
+	write(1, "\n", 1);
 }
 
