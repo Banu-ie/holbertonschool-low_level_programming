6bef2c1e69179a0e79c8a99af74368ab10362e44
Banu-ie <i.banu.4757@gmail.com>
2025-12-10 06:51:27 +0000

    .

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,24 +1,24 @@
 #include "main.h"
+#include <stddef.h> /* for NULL */
 
 /**
  * _strchr - locates a character in a string
  * @s: the string to search
  * @c: the character to find
  *
- * Return: pointer to the first occurrence of c in s, or 0 if not found
+ * Return: pointer to the first occurrence of c in s, or NULL if not found
  */
 char *_strchr(char *s, char c)
 {
-    while (*s)
-    {
-        if (*s == c)
-            return (s);
-        s++;
-    }
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
 
-    if (*s == c) /* check for terminating null byte */
-        return (s);
+	if (*s == c) /* check for terminating null byte */
+		return (s);
 
-    return (0);
+	return (NULL);
 }
-
