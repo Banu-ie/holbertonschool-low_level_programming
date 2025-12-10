576fcd930f100ad87048826543252fa39fc74eb8
Banu-ie <i.banu.4757@gmail.com>
2025-12-10 05:56:58 +0000

    Add 100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: input string
+ *
+ * Return: converted integer
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int sign = 1;
+	int num = 0;
+	int started = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = (num * 10) + (s[i] - '0');
+			started = 1;
+		}
+		else if (started == 1)
+			break;
+
+		i++;
+	}
+
+	return (num * sign);
+}
