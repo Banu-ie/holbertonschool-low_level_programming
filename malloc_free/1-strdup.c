577fee1bb043a93af05a971fd368b06a6ce43b49
Banu-ie <i.banu.4757@gmail.com>
2025-12-22 08:12:45 +0000

    Add 1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *_strdup(char *str)
+{
+    char *dup;
+    unsigned int i, len;
+    
+    if (str == NULL)
+        return(NULL);
+
+    len = 0;
+    while(str[len] != '\0')
+        len++;
+
+    dup =  malloc(sizeof(char) * (len + 1)):
+    if (dup == NULL)
+        return(NULL)
+
+    i = 0;
+    while(i <= len)
+    {
+        dup [i] = str[i];
+        i++;
+    }
+
+    return(dup);
+}
+
