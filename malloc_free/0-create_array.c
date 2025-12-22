fcb5fb5ad91ab52129dc8f1cef3a9d794baf9e33
Banu-ie <i.banu.4757@gmail.com>
2025-12-22 07:44:31 +0000

    Add 0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -8,24 +8,24 @@
  *
  * Return: pointer to the array, or NULL if it fails
  */
-char *create_array(unsigned int size, char c) 
+char *create_array(unsigned int size, char c)
 {
     char *array;
     unsigned int i;
-    
+
     if (size == 0)
         return (NULL);
-        
+
     array = malloc(sizeof(char) * size);
     if (array == NULL)
         return (NULL);
-        
+
     i = 0;
     while (i < size)
     {
         array[i] = c;
         i++;
     }
-        
+
     return (array);
-} 
+}
