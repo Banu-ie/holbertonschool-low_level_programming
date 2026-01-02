21db3651ca4d8f1e59788153f6e8fc39248a8cdf
Banu-ie <i.banu.4757@gmail.com>
2026-01-02 15:00:08 +0000

    Add 0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include <stddef.h> /* чтобы компилятор знал NULL */
+#include <stddef.h> /* для NULL */
 #include "function_pointers.h"
 
 /**
@@ -10,8 +10,8 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name == NULL || f == NULL)
-        return;
+	if (name == NULL || f == NULL)
+		return;
 
-    f(name);
+	f(name);
 }
