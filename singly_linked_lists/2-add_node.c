8b58173cc5782b308edb05065a6cd62870a1a90a
Banu-ie <i.banu.4757@gmail.com>
2026-01-17 17:34:37 +0000

    Add 2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,27 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
- * add_node - Adds a new node at the beginning of a list_t list.
- * @head: A double pointer to the list_t list.
- * @str: The string to be added to the new node.
+ * add_node - Adds a new node at the beginning of a list_t list
+ * @head: Pointer to pointer to the head of the list
+ * @str: String to be duplicated and added to the new node
  *
- * Return: The address of the new element, or NULL if it failed.
+ * Return: Address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 	unsigned int len = 0;
 
-	/* 1. Allocate memory for the new node */
+	if (head == NULL || str == NULL)
+		return (NULL);
+
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 
-	/* 2. Calculate the length of the string manually */
-	while (str && str[len])
-		len++;
-
-	/* 3. Duplicate the string into the node */
 	new->str = strdup(str);
 	if (new->str == NULL)
 	{
@@ -29,11 +28,11 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	/* 4. Initialize the members of the struct */
+	while (str[len])
+		len++;
+
 	new->len = len;
 	new->next = *head;
-
-	/* 5. Point the head of the list to our new node */
 	*head = new;
 
 	return (new);
