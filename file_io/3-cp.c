0f1ce996ed618e0289ffb8c4d7b6fc0ab037c596
Banu-ie <i.banu.4757@gmail.com>
2026-01-23 10:43:15 +0000

    Fix betty error

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,28 +5,23 @@
 
 #define BUF_SIZE 1024
 
-static void print_usage(void)
+static void err_exit(int code, const char *name, int fd)
 {
-	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-	exit(97);
+	if (code == 97)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	else if (code == 98)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", name);
+	else if (code == 99)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", name);
+	else if (code == 100)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(code);
 }
 
-static void error_read(const char *file)
+static void safe_close(int fd)
 {
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
-	exit(98);
-}
-
-static void error_write(const char *file)
-{
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
-	exit(99);
-}
-
-static void error_close(int fd)
-{
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-	exit(100);
+	if (close(fd) == -1)
+		err_exit(100, NULL, fd);
 }
 
 static void copy_file(int fd_from, int fd_to, char *from, char *to)
@@ -39,17 +34,16 @@ static void copy_file(int fd_from, int fd_to, char *from, char *to)
 		w = write(fd_to, buf, r);
 		if (w == -1 || w != r)
 		{
-			close(fd_from);
-			close(fd_to);
-			error_write(to);
+			safe_close(fd_from);
+			safe_close(fd_to);
+			err_exit(99, to, 0);
 		}
 	}
-
 	if (r == -1)
 	{
-		close(fd_from);
-		close(fd_to);
-		error_read(from);
+		safe_close(fd_from);
+		safe_close(fd_to);
+		err_exit(98, from, 0);
 	}
 }
 
@@ -58,26 +52,23 @@ int main(int ac, char **av)
 	int fd_from, fd_to;
 
 	if (ac != 3)
-		print_usage();
+		err_exit(97, NULL, 0);
 
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-		error_read(av[1]);
+		err_exit(98, av[1], 0);
 
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		close(fd_from);
-		error_write(av[2]);
+		safe_close(fd_from);
+		err_exit(99, av[2], 0);
 	}
 
 	copy_file(fd_from, fd_to, av[1], av[2]);
 
-	if (close(fd_from) == -1)
-		error_close(fd_from);
-
-	if (close(fd_to) == -1)
-		error_close(fd_to);
+	safe_close(fd_from);
+	safe_close(fd_to);
 
 	return (0);
 }
